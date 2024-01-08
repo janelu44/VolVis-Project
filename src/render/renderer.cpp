ef9fafd4a574f86f7194df69d7e811d4fab16209
janelu44 <matei.galesanu@gmail.com>
2024-01-08 23:22:13 +0100

    Basic iso-surface raycasting

--- src/render/renderer.cpp
+++ src/render/renderer.cpp
@@ -176,7 +176,16 @@ glm::vec4 Renderer::traceRayMIP(const Ray& ray, float sampleStep) const
 glm::vec4 Renderer::traceRayISO(const Ray& ray, float sampleStep) const
 {
     static constexpr glm::vec3 isoColor { 0.8f, 0.8f, 0.2f };
-    return glm::vec4(isoColor, 1.0f);
+    
+    glm::vec3 samplePos = ray.origin + ray.tmin * ray.direction;
+    const glm::vec3 increment = sampleStep * ray.direction;
+    for (float t = ray.tmin; t <= ray.tmax; t += sampleStep, samplePos += increment) {
+        const float val = m_pVolume->getSampleInterpolate(samplePos);
+        if (val > m_config.isoValue) {
+            return glm::vec4(isoColor, 1.0f); 
+        }
+    }
+    return glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
 }
 
 // ======= TODO: IMPLEMENT ========
