ebb5646951fe4be582152ba8d44abe7cb88d8992
A.P. Ivanov <a.p.ivanov@student.tudelft.nl>
2024-01-30 19:04:01 +0100

    Trilinear interpolation

--- src/volume/volume.cpp
+++ src/volume/volume.cpp
@@ -121,7 +121,18 @@ float Volume::getSampleNearestNeighbourInterpolation(const glm::vec3& coord) con
 // This function returns the trilinear interpolated value at the continuous 3D position given by coord.
 float Volume::getSampleTriLinearInterpolation(const glm::vec3& coord) const
 {
-    return 0.0f;
+    // check if the coordinate is within volume boundaries, since we only look at direct neighbours we only need to check within 0.5
+    if (glm::any(glm::lessThan(coord, glm::vec3(0))) || glm::any(glm::greaterThanEqual(coord + 1.0f, glm::vec3(m_dim))))
+        return 0.0f;
+
+    int lo = glm::floor(coord.z), hi = glm::ceil(coord.z);
+    auto factor = coord.z - lo;
+
+    return linearInterpolate(
+        biLinearInterpolate(glm::vec2(coord), lo),
+        biLinearInterpolate(glm::vec2(coord), hi),
+        factor
+    );
 }
 
 // This function linearly interpolates the value at X using incoming values g0 and g1 given a factor (equal to the positon of x in 1D)
@@ -130,13 +141,20 @@ float Volume::getSampleTriLinearInterpolation(const glm::vec3& coord) const
 //   factor
 float Volume::linearInterpolate(float g0, float g1, float factor)
 {
-    return 0.0f;
+    return g0 + factor * (g1 - g0);
 }
 
 // This function bi-linearly interpolates the value at the given continuous 2D XY coordinate for a fixed integer z coordinate.
 float Volume::biLinearInterpolate(const glm::vec2& xyCoord, int z) const
 {
-    return 0.0f;
+    auto lo = glm::floor(xyCoord), hi = glm::ceil(xyCoord);
+    auto factor = xyCoord - lo;
+
+    return linearInterpolate(
+        linearInterpolate(getVoxel(lo.x, lo.y, z), getVoxel(hi.x, lo.y, z), factor.x),
+        linearInterpolate(getVoxel(lo.x, hi.y, z), getVoxel(hi.x, hi.y, z), factor.x),
+        factor.y
+    );
 }
 
 
